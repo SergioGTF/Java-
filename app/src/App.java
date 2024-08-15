eb4db95544c97b4fe675a090b3a220e3fa1633d4
Sergiogtf <sergio23300033@aluno.cesupa.br>
2024-08-15 13:51:04 -0300

    Classes e método

--- app/src/App.java
+++ app/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.tampada = false;
+        c1.tampar();
+        c1.status();
     }
 }
---
+++ app/src/caneta.java
@@ -0,0 +1,27 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    
+    void status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Ponta " + this.ponta);
+        System.out.println("Carga " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+
+    void rabiscar(){
+        
+    }
+
+    void tampar(){
+        this.tampada = true;
+    }
+
+    void destampar(){
+        this.tampada = false;
+    }
+}
